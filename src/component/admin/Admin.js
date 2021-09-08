44b1048346910d296f4e9d7d9f6062506460c252
DaiHoang1114 <hoangquocdai1411@gmail.com>
2021-09-08 15:51:10 +0700

    modified admin.js

--- src/component/admin/Admin.js
+++ src/component/admin/Admin.js
@@ -2,6 +2,8 @@ import { Container, Row, Tab, Col, Tabs } from "react-bootstrap"
 import { TiEdit, TiDelete  } from "react-icons/ti"
 import {FaUserEdit} from "react-icons/fa"
 import {IconButton} from "@chakra-ui/react"
+import useApiGet from "../../hook/useApiGet"
+import LoadingSpinner from "../shared/LoadingSpinner"
 export default function Admin () {
     return (
         <div>
@@ -12,7 +14,7 @@ export default function Admin () {
 
 function SubMenu () {
     return (
-        <Container>
+        <Container className="mt-4">
             <Tabs defaultActiveKey="member" id="uncontrolled-tab-example" className="mb-3">
                 <Tab eventKey="member" title="Members">
                     <MemberComponent />
@@ -26,35 +28,40 @@ function SubMenu () {
 }
 
 function MemberComponent() {
+    const { data: accounts, loading, error} = useApiGet({defaultValue: [], endpoint: '/accounts'})
     return (
         <div>
             <Row>
-                <Col>Photo</Col>
+                <Col xs={1}>Photo</Col>
                 <Col>Member name</Col>
-                <Col>Mobile</Col>
                 <Col>Email</Col>
+                <Col>Mobile</Col>
                 <Col>Operation</Col>
             </Row>
-            {Array.from({length: 5}).map((_, idx) => (
-                <Row>
-                    <Col xs={1}>Picture</Col>
-                    <Col>George Lindelof</Col>
-                    <Col>+4 315 23 63</Col>
-                    <Col>george@gmail.com</Col>
-                    <Col>
-                    <IconButton
-                        isRound
-                        variant='ghost'
-                        icon={<FaUserEdit />}>
-                    </IconButton>
-                    <IconButton
-                        isRound
-                        variant='ghost'
-                        icon={<TiDelete />}>
-                    </IconButton>
-                    </Col>
-                </Row>
-            ))}
+            {loading ? <LoadingSpinner /> :
+                <Container>
+                {accounts.map(({ id, name, email, phoneNumber}) => (
+                    <Row key={id}>
+                        <Col xs={1}>Picture</Col>
+                        <Col>{name}</Col>
+                        <Col>{email}</Col>
+                        <Col>{phoneNumber}</Col>
+                        <Col>
+                            <IconButton
+                                isRound
+                                variant='ghost'
+                                icon={<FaUserEdit />}>
+                            </IconButton>
+                            <IconButton
+                                isRound
+                                variant='ghost'
+                                icon={<TiDelete />}>
+                            </IconButton>
+                        </Col>
+                    </Row>
+                ))}
+                </Container>
+            }
         </div>
     )
 }
