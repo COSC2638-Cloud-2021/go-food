b1425ed65b710de0db90bea5d0f7bfe9ff9442ce
DaiHoang1114 <hoangquocdai1411@gmail.com>
2021-09-09 09:37:46 +0700

    modified support.js

--- src/component/support/Support.js
+++ src/component/support/Support.js
@@ -1,5 +1,35 @@
+import { Heading, FormControl, FormLabel, Input, Button, Select, FormHelperText } from "@chakra-ui/react"
+import { Container } from "react-bootstrap"
 export default function Support () {
     return (
-        <div>This is support page</div>
+        <Container>
+            <Heading>Please fill in the form bellow and then we will contact you soon</Heading>
+            <FormControl id="name" isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input placeholder="Name" />
+                </FormControl>
+                <FormControl id="email" isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input placeholder="Email" />
+                <FormHelperText>We'll never share your email.</FormHelperText>
+                </FormControl>
+                <FormControl id="phoneNumber" isRequired>
+                <FormLabel>Phone Number</FormLabel>
+                <Input placeholder="Phone Number" />
+                <FormHelperText>We'll never share your phone number.</FormHelperText>
+                </FormControl>
+                <FormControl id="role" isRequired>
+                <FormLabel>My role</FormLabel>
+                <Select placeholder="Please select one">
+                    <option>Customer</option>
+                    <option>Store owner</option>
+                </Select>
+                </FormControl>
+                <Button
+                    mt={4}
+                    colorScheme="teal"
+                    // isLoading={props.isSubmitting}
+                    type="submit">Submit</Button>
+        </Container>
     )
 }
