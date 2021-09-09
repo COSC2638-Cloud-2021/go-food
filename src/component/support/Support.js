3d4b1c12dcfb4466194097e533568b470f659c60
DaiHoang1114 <hoangquocdai1411@gmail.com>
2021-09-09 10:00:59 +0700

    modified support.js

--- src/component/support/Support.js
+++ src/component/support/Support.js
@@ -1,4 +1,4 @@
-import { Heading, FormControl, FormLabel, Input, Button, Select, FormHelperText } from "@chakra-ui/react"
+import { Heading, FormControl, FormLabel, Input, Button, Select, FormHelperText, Textarea } from "@chakra-ui/react"
 import { Container } from "react-bootstrap"
 export default function Support () {
     return (
@@ -7,29 +7,35 @@ export default function Support () {
             <FormControl id="name" isRequired>
                 <FormLabel>Name</FormLabel>
                 <Input placeholder="Name" />
-                </FormControl>
-                <FormControl id="email" isRequired>
+            </FormControl>
+            <FormControl id="email" isRequired>
                 <FormLabel>Email</FormLabel>
                 <Input placeholder="Email" />
                 <FormHelperText>We'll never share your email.</FormHelperText>
-                </FormControl>
-                <FormControl id="phoneNumber" isRequired>
+            </FormControl>
+            <FormControl id="phoneNumber" isRequired>
                 <FormLabel>Phone Number</FormLabel>
                 <Input placeholder="Phone Number" />
                 <FormHelperText>We'll never share your phone number.</FormHelperText>
-                </FormControl>
-                <FormControl id="role" isRequired>
+            </FormControl>
+            <FormControl id="role" isRequired>
                 <FormLabel>My role</FormLabel>
+                
                 <Select placeholder="Please select one">
                     <option>Customer</option>
                     <option>Store owner</option>
                 </Select>
-                </FormControl>
-                <Button
-                    mt={4}
-                    colorScheme="teal"
-                    // isLoading={props.isSubmitting}
-                    type="submit">Submit</Button>
+            </FormControl>
+            <FormControl id="message" isRequired>
+                <FormLabel>Message</FormLabel>
+                <Textarea placeholder="Please type your message here" />
+            </FormControl>
+                
+            <Button
+                mt={4}
+                colorScheme="teal"
+                // isLoading={props.isSubmitting}
+                type="submit">Submit</Button>
         </Container>
     )
 }
