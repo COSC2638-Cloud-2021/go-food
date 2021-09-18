fa0ec677ffe1f38fc9e8d511f5d01133e4d7cc32
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-09-18 11:14:12 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { HelmetProvider } from 'react-helmet-async';
 import AppRouter from './AppRouter';
 import theme from './theme';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
 import useAuthStore from './store/useAuthStore';
 
